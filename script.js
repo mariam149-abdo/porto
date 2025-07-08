d5f474aad0e3303adbb139da275c57037d49c603
mariam <mariamabdelhaq883@gmail.com>
2025-07-08 21:42:48 +0300

    edit porto

--- script.js
+++ script.js
@@ -44,4 +44,18 @@ window.addEventListener("scroll", ()=>{
   });
   });
 
-
+const menu = document.getElementById("mobile-menu");
+const menubtn= document.getElementById("menu-icon");
+const menua =document.querySelectorAll(".mobile-menu a");
+const animation ="animation-menu";
+menubtn.addEventListener("click", () => {
+ if (menu.style.display === "flex") {
+    menu.style.display = "none";
+    menua.forEach(link =>{link.style.display="none"});
+  } 
+  else {
+    menu.style.display = "flex";
+    menua.forEach(link =>{link.style.display="flex"});
+    menu.classList.add(animation);
+  }
+}) 
